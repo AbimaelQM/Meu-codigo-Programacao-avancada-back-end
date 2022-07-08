881d4d569c3758abf4ef1ec532ae9ff6c5810c94
AbimaelQM <60951913+AbimaelQM@users.noreply.github.com>
2022-07-08 15:21:17 -0500

    Alterando o back para usar express com mongoose

--- project-02/api/index.js
+++ project-02/api/index.js
@@ -1,47 +1,79 @@
-const http = require('http')
-const URL = require('url')
-const fs = require('fs')
-const path = require('path')
+// Configuração inicial
+const express = require("express")
+const app = express()
 
-const data = require('./urls.json')
+//Forma de ler JSON
+app.use(
+    express.urlencoded({
+        extended:true,
+    }),
+)
 
-http.createServer((req, res) => {
+app.use(express.json())
+
+
+// Rota incial /endpoint
+app.get('/',(req,res)=>{
+    // Mostrar requisição
+
+    res.json({message: "Oi express!"})
+})
+
+
+
+
+// entregar uma porta
+
+app.listen(3000)
+
+
+// /*
+// Esse codigo cria uma API para servir ao frontend da aplicação que simula o favoritos do navegador. 
+// */
+
+// // Importação dos módulos.
+// const http = require('http')
+// const URL = require('url')
+// const fs = require('fs')
+// const path = require('path')
+
+// // Importação do documento urls.json. Nesse momento do código, o conteúdo do documento vai para o objeto data.
+// const data = require('./urls.json')
+
+// // Servidor rodando na porta 3000.
+// http.createServer((req, res) => {
     
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,DELETE,UPDATE')
-    res.setHeader('Access-Control-Max-Age', '256100')
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     res.setHeader('Access-Control-Allow-Methods', 'POST,GET,DELETE,UPDATE')
+//     res.setHeader('Access-Control-Max-Age', '256100')
 
-    // res.writeHead(
-    //     200,
-    //     {"Access-Control-Allow-Origin": "*"}
-    // )
+//     // Desestruturação da query string da URL para obter os valores que estão associados às chaves name, url e del.
+//     const { name, url, del } = URL.parse(req.url, true).query
 
+//     // Função para escrever no documento JSON. Tanto no delete quanto no create. 
+//     function writeFile(cb) {
+//         fs.writeFile(
+//             path.join(__dirname, 'urls.json'),
+//             JSON.stringify(data, null, 2),
+//             err => {
+//                 if (err) throw err
+//                 res.end('Operação realizada com sucesso!')
+//             }
+//         )
+//     }
 
-    const { name, url, del } = URL.parse(req.url, true).query
+//     // Mostrar o conteúdo do JSON.
+//     if (!name || !url)
+//         return res.end(JSON.stringify(data))
 
-    function writeFile(cb) {
-        fs.writeFile(
-            path.join(__dirname, 'urls.json'),
-            JSON.stringify(data, null, 2),
-            err => {
-                if (err) throw err
-                cb(JSON.stringify(data))
-            }
-        )
-    }
+//     // Delete - apagar do JSON.
+//     if (del) {
+//         data.urls = data.urls.filter(item => item.url != url)
+//         return writeFile(message => res.end(message))
+//     }
 
+//     // Create - inserir no JSON.
+//     data.urls.push({name, url})
+//     return writeFile(message => res.end(message))
 
-    if (!name || !url) {
-        // return res.end('show')
-        return res.end(JSON.stringify(data))
-    }
-    if (del) {
-        data.urls = data.urls.filter(item => item.url != url)
-        writeFile(message => res.end(message))
-        return data.urls
-    }
-    data.urls.push({name, url})
-    writeFile(message => res.end(message))
-    return data.urls
-
-}).listen(3000, () => console.log('Api is running.'))
+// }).listen(3000, () => console.log('Api is running.'))
--- project-02/public/script.js
+++ project-02/public/script.js
@@ -3,7 +3,7 @@ const input = document.querySelector('#input')
 const form = document.querySelector('#form')
 
 
-window.addEventListener('load', e =>{
+window.addEventListener('load', e => {
     fetch(`http://localhost:3000/`)
         .then(content => content.json())
         .then(data => data.urls.map(({name, url}) => showElement({name, url})))
@@ -14,11 +14,9 @@ function showElement({ name, url }) {
     const li = document.createElement('li')
     li.innerHTML = `<a target="_blank" href="${url}">${name}</a> <button onClick="removeElement(this)" class="remove">X</button>`
     li.classList.add("li-listener")
-    // li = `<li class="li-listener" id=""> <a href="${url}">${name}: </a> <button class="btn btn-danger" id="exclui-fav">X</button></li>`
     ul.appendChild(li)
-    // ul.innerHTML = li
-
 }
+
 
 function addElement({name, url}){
 
@@ -27,16 +25,10 @@ function addElement({name, url}){
 }
 
 function removeElement(element) {
-    // console.log(element.parentNode)
-    // console.log(element.parentNode.children[0])
     const name = element.parentNode.children[0].textContent
     const href = element.parentNode.children[0].href
     const url = href.substring(0, href.length - 1);
-    // console.log(url)
     fetch(`http://localhost:3000/?name=${name}&url=${url}&del=1`)
-    // element.parentNode.style['visibility'] = 'hidden'
-    // element.parentNode.remove()
-
 }
 
 form.addEventListener('submit', (event) => {
@@ -57,31 +49,8 @@ form.addEventListener('submit', (event) => {
     if (/!^http/.test(url))
         return alert('Digite a url da maneira correta.')
 
-    console.log("add")
     addElement({ name, url })
     input.value = ''
     input.style['border'] = '2px inset rgb(133, 133, 133)'
 
 })
-
-
-// form.addEventListener('submit', (event) => {
-//     event.preventDefault();
-
-//     let { value } = input
-
-//     if (!value)
-//         return alert('Preencha o campo!')
-
-//     const [name, url] = value.split(',')
-
-//     if (!url)
-//         return alert('O texto não está formatado da maneira correta.')
-
-//     if (!/^http/.test(url))
-//         return alert('Digite a url da maneira correta.')
-
-//     addElement({ name, url })
-
-//     input.value = ''
-// })
